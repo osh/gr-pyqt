5fc311d886187e8b0cb27be77d5c65a1bb26d280
Tim O'Shea <tim.oshea753@gmail.com>
2014-03-23 20:39:40 -0700

    adding value histogram plotter widget

--- python/__init__.py
+++ python/__init__.py
@@ -53,6 +53,7 @@ from ctime_plot import *;
 from cpsd_plot import *;
 from time_plot import *;
 from value_plot import *;
+from value_hist import *;
 from cpower_plot import *;
 from pdu_meta_extract import *;
 
---
+++ python/value_hist.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from plotter_base import *
+class value_hist(plotter_base):
+    def __init__(self, *args):
+        plotter_base.__init__(self, *args)
+        self.message_port_register_in(pmt.intern("reals"));
+        self.set_msg_handler(pmt.intern("reals"), self.handler);
+
+        # set up curves
+        curve = Qwt.QwtPlotCurve("x(n)");
+        curve.attach(self);
+        self.curves.append(curve);
+        curve.setPen( Qt.QPen(Qt.Qt.green) );
+        
+        self.curve_data = [([],[])]
+        self.hist = [];
+
+    def handler(self, msg):
+        # get input
+        x = float(pmt.to_python(msg))
+        self.hist.append(x);
+        (density, domain) = numpy.histogram(self.hist,100);
+
+        # pass data
+        self.curve_data[0] = (domain, density);
+
+        # trigger update
+        self.emit(QtCore.SIGNAL("updatePlot(int)"), 0)
+
+
