d46b7957a149e7cce05742a12b04bcccc0bd9ba1
Tim O'Shea <tim.oshea753@gmail.com>
2014-03-24 13:44:56 -0700

    updating comments

--- python/pdu_meta_extract.py
+++ python/pdu_meta_extract.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python
+#
+# Copyright 2014 Tim O'Shea
+#
+# This file is part of GNU Radio
+#
+# GNU Radio is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# GNU Radio is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GNU Radio; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+#
 import pmt
 from gnuradio import gr
-
 class pdu_meta_extract(gr.sync_block):
     def __init__(self, key):
         gr.sync_block.__init__(self,"pdu_meta_extract",[],[])
